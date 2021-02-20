0c5cac84b7baa4d721d63c791af7001968ed2fee
Jidanur <jidanurrahman99@gmail.com>
2021-02-19 20:49:24 -0600

    Update Train.java

--- src/Train.java
+++ src/Train.java
@@ -89,13 +89,42 @@ public class Train{
 	}
 	
 	//add the car by index, dont know how to write, this is pretty similar as 2150's a1
-	public void findIndexAdd(int index) {
-		if(isEmpty()) {
+	public boolean findIndexAdd(int index, int value) {
+	
+		boolean added = false;
+		
+		if(index == 0 ) {
+			addHead(value);
+			added = true;
+		}
+		else if(index==numTrains){
+			addTail(value);
+			added = true;
+		}
+		else {
+			Node curr = head;
+			int currInx =0;
+			while(curr != null && !added) {
+				
+				if(currInx == index) {
+					added = true;
+				}
+				curr = curr.getNext();
+				currInx++;
+			}
+			
+			if(index<numTrains && curr!= null) {
+				Node prev = curr.getPrev();
+				prev.setNext(curr.getNext());
+				added = true;
+			}
 			
 		}
 		
-		numTrains++;
-	}
+		
+		return added;
+		}
+		
 	
 	
 	/*
@@ -107,37 +136,7 @@ public class Train{
 	 * 
 	 */
 	//both of them don't work
-	public boolean searchUnordered(int index) {
-		boolean found = false;
-		Node curr = head;
-		
-		while(curr!=null) {
-			if(curr.getIndex()== index) {
-				
-				found = true;
-			}else {
-				curr = curr.getNext();
-			}
-		}		
-		return found;
-	}
 	
-	public int searchOrdered(int index) {
-		//boolean found = false;
-		int i = 1;
-		Node curr = head;
-		
-		while(curr!=null) {
-			if(curr.getIndex()==index)
-				//found = true;
-				i = curr.getIndex();
-			else
-				i++;
-				curr = curr.getNext();
-		}
-		
-		return i;
-	}
 	
 	/*
 	 * Delete
@@ -148,27 +147,34 @@ public class Train{
 	//cannot delete
 	public void deleteCar(int index) {
 		Node curr = head;
-		Node prev = null;
+		int currInx = 0;
+		
 		
 		if(isEmpty()) {
-			System.out.println("Delete Fails! No any cars in this train...");
-		}else {
-			//find where is the key
-			while(curr!=null&& curr.getIndex()!=index) {
-				prev = curr;
-				curr = curr.getNext();
+			System.out.println("Delete Fails! No any cars in this train6...");
+		}
+		else if(index==0 ) {
+			head = head.getNext(); 
+		}
+		
+		else if(index==this.numTrains){
+			
+			Node prev= tail.getPrev();
+			prev.setNext(null);
+			tail = prev;
+		}
+		
+		else {
+			while(curr!= null && currInx < index) {
+				
+				curr= curr.getNext();
+				currInx++;
 			}
 			
-			//if we found the key....
-			if(curr!=null) {
-				if(prev!=null) {
-					System.out.println(curr.getIndex());
-					prev.setNext(curr.getNext());
-				}else {
-					head = curr.getNext();
-				}
+			if(curr!= null && currInx== index) {
+				Node prev = curr.getPrev();
+				prev.setNext(curr.getNext());
 			}
-			
 		}
 	}
 	
